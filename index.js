66f92626646f3a4d4e19ccbaa3c3161df612b68c
nhuse <nathan.huse@yahoo.com>
2021-06-28 12:44:10 -0500

    commit tree dfc2a2992666ed16fe8766a0fbe202efcfebf640

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 100) + 1;
+
+let num3 = 9;
+let num4 = 5;
+let mod = num3%num4;
+
+let max = Math.max(5, 2, 18, 8, 20, 12, 17);
